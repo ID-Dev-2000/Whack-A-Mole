a748327f9232cdb0afab80c8d419d37de9414603
ID <izaakduncan@outlook.com>
2022-06-16 21:23:36 -0700

    dynamic grid size, mobile responsiveness

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ https://id-dev-2000.github.io/Whack-A-Mole/
 
 Whack-A-Mole
 
-Final: added variable difficulty settings, and handled critical errors
+Mobile Responsiveness only works on load
--- script.js
+++ script.js
@@ -1,10 +1,45 @@
 let mainGridBox = document.getElementById('gridBox')
+let gridSizeSelect = document.getElementById('gridSizeSelect')
+let gridSizeOptions = document.getElementById('gridSizeOptions')
 
-for (i=1; i <= 3; i++) {
+let gridSizeVariable = 3
+
+gridSizeSelect.addEventListener('click', function() {
+    // Mobile responsiveness: remove grid size options, set grid to default (3)
+    let mediaQuery = window.matchMedia('(max-width: 500px)')
+
+    // Sub-optimal solution, should use slice alternative
+    if (mediaQuery.matches) {
+        gridSizeSelect.options[3].hidden = true
+        gridSizeSelect.options[4].hidden = true
+        gridSizeSelect.options[5].hidden = true
+    }
+})
+
+gridSizeSelect.addEventListener('change', function() {
+    gridSizeVariable = gridSizeSelect.options[gridSizeSelect.selectedIndex].value
+    // update grid box upon user input
+    mainGridBox.innerHTML = ''
+    for (i=1; i <= gridSizeVariable; i++) { 
+        let lineRowDiv = document.createElement('div')
+        lineRowDiv.classList.add(`gridContainer${i}`)
+        mainGridBox.appendChild(lineRowDiv)
+        for (j=1; j <= gridSizeVariable; j++) {
+            let identityValue = lineRowDiv.className.slice(-1)
+            let singleBoxAsDiv = document.createElement(`div`)
+            singleBoxAsDiv.setAttribute('id', `i${(identityValue * j) * j}`)
+            singleBoxAsDiv.classList.add('gridSquare')
+            lineRowDiv.appendChild(singleBoxAsDiv)
+        }
+    }
+})
+
+// Draw grid box on initial load
+for (i=1; i <= gridSizeVariable; i++) {
     let lineRowDiv = document.createElement('div')
     lineRowDiv.classList.add(`gridContainer${i}`)
     mainGridBox.appendChild(lineRowDiv)
-    for (j=1; j <= 3; j++) {
+    for (j=1; j <= gridSizeVariable; j++) {
         let identityValue = lineRowDiv.className.slice(-1)
         let singleBoxAsDiv = document.createElement(`div`)
         singleBoxAsDiv.setAttribute('id', `i${(identityValue * j) * j}`)
@@ -24,18 +59,25 @@ function pointHandler() {
 }
 
 function handleColorInGridItem() {
-    let randomlySelectedRowValue = Math.floor(Math.random() * 3)
-    let randomlySelectedColumnValue = Math.floor(Math.random() * 3)
+    // Randomly selects grid item
+    let randomlySelectedRowValue = Math.floor(Math.random() * gridSizeVariable)
+    let randomlySelectedColumnValue = Math.floor(Math.random() * gridSizeVariable)
+    // Creates clone of grid item, used to prevent infinite clicks, removes event listener
     let gridItemToBeTemporarilyModified = mainGridBox.children[randomlySelectedRowValue].children[randomlySelectedColumnValue]
     let gridItemClone = gridItemToBeTemporarilyModified.cloneNode(true)
     gridItemToBeTemporarilyModified.style.backgroundColor = 'lightBlue'
-    gridItemToBeTemporarilyModified.addEventListener('click', pointHandler, {once: true})
+    gridItemToBeTemporarilyModified.addEventListener('click', function() {
+        pointHandler()
+        gridItemToBeTemporarilyModified.style.backgroundColor = 'lightGreen'
+    }, {once: true})
     setTimeout(function() {
         gridItemToBeTemporarilyModified.style.backgroundColor = 'white'
+        // Replaces grid item after color changes
         gridItemToBeTemporarilyModified.parentNode.replaceChild(gridItemClone, gridItemToBeTemporarilyModified)
     }, difficultySlider.value)
 }
 
+// Starts game, disables play button to indicate to user that game has began
 playButton.addEventListener('click', function() {
     playButton.disabled = true
     gamePoints = 0
